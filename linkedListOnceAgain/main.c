b3ab8b516b2dfd7e25c8b344f6cc66e923de0165
Sun PeiFeng <g7733pf@gmail.com>
2013-12-11 23:22:51 +0800

    照书上打的代码233

--- linkedListOnceAgain/main.c
+++ linkedListOnceAgain/main.c
@@ -9,179 +9,179 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #define SIZE_OF_STACK 100
-#define STACK_INCREASEMENT 10
+#define OK 1
+#define ERROR 0
+#define STACK_INIT_SIZE 100 // 存储空间初始分配量
+#define STACKINCREMENT 10 // 存储空间分配增量
 
-typedef struct oneAlsoTwoLinkedStructure{
-    
-    int data;
-    struct oneAlsoTwoLinkedStructure *next;
-    struct oneAlsoTwoLinkedStructure *prev;
-    
-}oneAlsoTwoLinkedStructure, *OneWayLinkedList, *TwoWaysLinkedList;
-
+typedef int SElemType; // 定义栈元素类型
+typedef int Status; // Status是函数的类型,其值是函数结果状态代码，如OK等
+//typedef struct oneAlsoTwoLinkedStructure{
+//    
+//    int data;
+//    struct oneAlsoTwoLinkedStructure *next;
+//    struct oneAlsoTwoLinkedStructure *prev;
+//    
+//}oneAlsoTwoLinkedStructure, *OneWayLinkedList, *TwoWaysLinkedList;
+//
 typedef int DataType;
 
-typedef struct structOfStack{
-    
+
+typedef struct{
+    SElemType *base;
+    SElemType *top;
     int stackSize;
-    int *base;
-    int *top;
-    
-}structOfStack, *StackToOperate;
+} Sqstack;
 
-//StackToOperate pushElementIntoStack(int lengthOfTheCreatingStack)
-//{
-//    StackToOperate stack,q,p;
-//    stack = (StackToOperate)malloc(sizeof(structOfStack));
-//    stack->data = 0;
-//    stack->top = NULL;
-//    stack->base = stack;
-//
-//}
 
-int initStack(structOfStack &s)
+
+Status initStack(Sqstack &s)//这里提示&的使用有问题.....剩下几个函数的问题类似
 {
-    s.base = (int *)malloc(sizeof(int)*SIZE_OF_STACK);
+    s.base = (SElemType *)malloc(STACK_INIT_SIZE * sizeof(SElemType));
     if (!s.base)
     {
-        printf("Failed to find s->base");
         exit(100);
     }
     s.top = s.base;
-    s.stackSize = SIZE_OF_STACK;
-    
-    return s;
+    s.stackSize = STACK_INIT_SIZE;
+    return OK;
 }
 
-int getTopElement(StackToOperate s,int e)
+Status getTop(Sqstack s,SElemType &e)
 {
-    if (s->top == s->base)
+    if (s.top == s.base)
     {
-        printf("Stack is EMPTY!");
-        exit(101);
+        e = *(s.top - 1);
+        return OK;
     }
-    e = (s->top)-1;
-    return e;
 }
 
-StackToOperate pushElementIn(StackToOperate s,int e)
+Status push(Sqstack &s,SElemType e)
 {
-    if ((s->top)-(s->base) >= s->stackSize)
+    if (s.top-s.base >= s.stackSize)
     {
-        s->base = (StackToOperate)realloc(s->base,sizeof(StackToOperate)*(s->stackSize + STACK_INCREASEMENT));
-        if (!s->base)
+        s.base = (SElemType)realloc(s.base, (s.stackSize + STACKINCREMENT)*sizeof(SElemType));
+        if (!s.base)
         {
-            exit(102);
+            exit(101);
         }
-        s->top = s->base + s->stackSize;
-        s->stackSize +=STACK_INCREASEMENT;
+        s.top = s.base + s.stackSize;
+        s.stackSize += STACKINCREMENT;
     }
-    
-    s->top = e;
-    s
-    
+    *s.top++ = e;
+    return OK;
 }
 
-
-
-
-OneWayLinkedList initOneWayLinkedList(int lengthOfTheCreatingList)
+Status pop(Sqstack &s,SElemType e)
 {
-    OneWayLinkedList p = NULL, r =NULL, list = NULL;
-    
-    
-    //p负责建立新节点,r负责临时存储
-    
-    list = (OneWayLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));
-    list->data = 0;
-    list->next = NULL;
-    list->prev = NULL;
-    
-    for (int i = 1; i <= lengthOfTheCreatingList; i++)
+    if (s.top == s.base)
     {
-        p = (OneWayLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));
-        //r = (OneWayLinkedList)malloc(sizeof(oneWayLinkedStructure));
-        
-        p->data = i;
-        p->next = NULL;
-        p->prev = NULL;
-        
-        if (i == 1)
-        {
-            list->next = p;
-        }
-        else if (i >= 2)
-        {
-            r->next = p;
-        }
-        
-        r = p;
-        
+        return ERROR;
     }
-    
-    return list;
+    e=*--s.top;
+    return OK;
 }
 
 
-TwoWaysLinkedList initTwoWaysLinkedList(int lengthOfTheCreatingList)
-{
-    TwoWaysLinkedList p = NULL, q = NULL, list = NULL;
-    
-    list = (TwoWaysLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));
-    list->data = 0;
-    list->next = NULL;
-    list->prev = NULL;//初始化list的第一个数据，
-    
-    //从此处开始进入循环
-    for (int i = 1;i <= lengthOfTheCreatingList; i++)
-    {
-        p = (TwoWaysLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));//每次循环都新建节点p
-        p->data = i;//给每次新建的p赋值
-        p->next = NULL;//初始化每次新建的p
-        p->prev = NULL;
-        
-        if (i == 1)
-        {
-            list->next = p;//如果是第一次循环,把list->next指向的地址设为p
-        }
-        else
-        {
-            q->next = p;//q此时即为上次循环中的list->next节点,在本次循环中只需要把本次新建的p赋值给q->next(即为list->next->next)
-            p->prev = q;//q此时即为上次循环中的list->next节点,本句即是设置q为p的前节点(即list->next->prev)
-        }
-        
-        q = p;//无论是第几次循环,都把q的地址存在p
-        //q此时即为本次循环中的list->next节点,在下次循环中只需要把下次新建的p赋值给q->next(即为list->next->next)
-        
-    }
-    
-    return list;
-    
-}
 
-
-void printOneWayLinkedList(oneAlsoTwoLinkedStructure *listToBePrinted)
-{
-    OneWayLinkedList p = NULL;
-    p = listToBePrinted;
-    while (p->next)
-    {
-        printf("%i\n",p->data);
-        p = p->next;
-    }
-    printf("%i\n",p->data);
-}
-
-
-void convertMyLinkedListToConverse(oneAlsoTwoLinkedStructure *listToBeConverted)
-{
-    oneAlsoTwoLinkedStructure *p = NULL;
-    
-    
-}
-
-
+//OneWayLinkedList initOneWayLinkedList(int lengthOfTheCreatingList)
+//{
+//    OneWayLinkedList p = NULL, r =NULL, list = NULL;
+//    
+//    
+//    //p负责建立新节点,r负责临时存储
+//    
+//    list = (OneWayLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));
+//    list->data = 0;
+//    list->next = NULL;
+//    list->prev = NULL;
+//    
+//    for (int i = 1; i <= lengthOfTheCreatingList; i++)
+//    {
+//        p = (OneWayLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));
+//        //r = (OneWayLinkedList)malloc(sizeof(oneWayLinkedStructure));
+//        
+//        p->data = i;
+//        p->next = NULL;
+//        p->prev = NULL;
+//        
+//        if (i == 1)
+//        {
+//            list->next = p;
+//        }
+//        else if (i >= 2)
+//        {
+//            r->next = p;
+//        }
+//        
+//        r = p;
+//        
+//    }
+//    
+//    return list;
+//}
+//
+//
+//TwoWaysLinkedList initTwoWaysLinkedList(int lengthOfTheCreatingList)
+//{
+//    TwoWaysLinkedList p = NULL, q = NULL, list = NULL;
+//    
+//    list = (TwoWaysLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));
+//    list->data = 0;
+//    list->next = NULL;
+//    list->prev = NULL;//初始化list的第一个数据，
+//    
+//    //从此处开始进入循环
+//    for (int i = 1;i <= lengthOfTheCreatingList; i++)
+//    {
+//        p = (TwoWaysLinkedList)malloc(sizeof(oneAlsoTwoLinkedStructure));//每次循环都新建节点p
+//        p->data = i;//给每次新建的p赋值
+//        p->next = NULL;//初始化每次新建的p
+//        p->prev = NULL;
+//        
+//        if (i == 1)
+//        {
+//            list->next = p;//如果是第一次循环,把list->next指向的地址设为p
+//        }
+//        else
+//        {
+//            q->next = p;//q此时即为上次循环中的list->next节点,在本次循环中只需要把本次新建的p赋值给q->next(即为list->next->next)
+//            p->prev = q;//q此时即为上次循环中的list->next节点,本句即是设置q为p的前节点(即list->next->prev)
+//        }
+//        
+//        q = p;//无论是第几次循环,都把q的地址存在p
+//        //q此时即为本次循环中的list->next节点,在下次循环中只需要把下次新建的p赋值给q->next(即为list->next->next)
+//        
+//    }
+//    
+//    return list;
+//    
+//}
+//
+//
+//void printOneWayLinkedList(oneAlsoTwoLinkedStructure *listToBePrinted)
+//{
+//    OneWayLinkedList p = NULL;
+//    p = listToBePrinted;
+//    while (p->next)
+//    {
+//        printf("%i\n",p->data);
+//        p = p->next;
+//    }
+//    printf("%i\n",p->data);
+//}
+//
+//
+//void convertMyLinkedListToConverse(oneAlsoTwoLinkedStructure *listToBeConverted)
+//{
+//    oneAlsoTwoLinkedStructure *p = NULL;
+//    
+//    
+//}
+//
+//
 
 
 
@@ -189,20 +189,15 @@ void convertMyLinkedListToConverse(oneAlsoTwoLinkedStructure *listToBeConverted)
 int main(int argc, const char * argv[])
 {
     
-//    printOneWayLinkedList(initOneWayLinkedList(5));
-//    printOneWayLinkedList(initTwoWaysLinkedList(5));
-//    printf("The first num is %i, and 2nd num is %i, its  prev is %i\n",
-//    
-//            (int)initTwoWaysLinkedList(5)->next->data,
-//            (int)initTwoWaysLinkedList(5)->next->next->data,
-//            (int)initTwoWaysLinkedList(5)->next->next->prev->data
-//    
-//           );
-    structOfStack *s;
-    int o,l = 0;
-    s = (StackToOperate)malloc(sizeof(StackToOperate));
-    o = getTopElement(initStack(*s), l);
-    printf("%i%i",o,l);
+    //printOneWayLinkedList(initOneWayLinkedList(5));
+    //printOneWayLinkedList(initTwoWaysLinkedList(5));
+    // printf("The first num is %i, and 2nd num is %i, its  prev is %i\n",
+    
+    //        (int)initTwoWaysLinkedList(5)->next->data,
+    //        (int)initTwoWaysLinkedList(5)->next->next->data,
+    //        (int)initTwoWaysLinkedList(5)->next->next->prev->data
+    
+    //       );
     
     return 0;
     
